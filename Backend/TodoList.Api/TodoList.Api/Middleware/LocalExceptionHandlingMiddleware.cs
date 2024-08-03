7034667fb643769f52687e702f137bba7c711c4a
Rob Lancaster <rilancaster@outlook.co.nz>
2024-08-04 11:08:12 +1200

    Log exeptions to disc...

--- Backend/TodoList.Api/TodoList.Api/Middleware/LocalExceptionHandlingMiddleware.cs
+++ Backend/TodoList.Api/TodoList.Api/Middleware/LocalExceptionHandlingMiddleware.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using UuidExtensions;
 
 namespace TodoList.Api.Middleware;
 
@@ -15,16 +18,23 @@ public class LocalExceptionHandlingMiddleware : IMiddleware
 
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
+        // TODO: get trace id from dotnet.. not a Guid...
+        var transactionId = Uuid7.Guid();
+
+        using (_logger.BeginScope(new List<KeyValuePair<string, object>>
+        {
+            new KeyValuePair<string, object>("TransactionId", transactionId),
+        }))
+
         try
         {
             await next(context);
         }
-        catch (System.Exception ex)
+        catch (Exception ex)
         {
-            _logger.LogError(ex, ex.Message);
-
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
 
+            // Not a good example of Semantic Logging...
             _logger.LogError("Error {Message} {Exception}", ex.Message, ex);
 
             await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
